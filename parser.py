1222bbe2827444bc5d1ce33b5b9988cb9549370e
Chinwendu <chinwenduukoha@gmail.com>
2022-05-02 15:00:34 -0400

    look over it

--- parser.py
+++ parser.py
@@ -18,3 +18,24 @@ employee_list = []
 # Takes in the path of the csv file and an output path
 # for creating a new JSON file as arguments
 # creates a new JSON file with formatted csv data
+
+
+def csv_to_json(csv_path, json_path):
+    with open(csv_path, mode='r', encoding='utf-8-sig') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        for entry in csv_data:
+            read_data.append(tuple(entry))
+    headers = read_data[0]
+    employees = [read_data[i] for i in range(1, len(read_data))]
+    for name in headers:
+        fields.append(name.replace('', '_').lower())
+
+    for employee in employees:
+        employee_json = {}
+        for i in range(len(employee)):
+            employee_json[fields[i]] = employee[i]
+        employee_list.append(employee_json)
+
+
+csv_to_json('data/employee_data.csv', 'data/employees.json')
+print(employee_list[2])
