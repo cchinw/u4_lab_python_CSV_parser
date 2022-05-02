4d29382d2c9ed21cebb860f46507febeba61f10c
Chinwendu <chinwenduukoha@gmail.com>
2022-05-02 13:59:17 -0400

    first part done

--- parser.py
+++ parser.py
@@ -1,24 +1,20 @@
 ####################################################
 #  python library imports here: csv, json
 
-
-
-
+import csv
+import json
 
 ####################################################
-# Set up 3 global variables to process the csv file 
+# Set up 3 global variables to process the csv file
 # and convert it to JSON
 # All variables will be empty lists to start: []
 
-
-
-
+read_data = []
+fields = []
+employee_list = []
 
 ####################################################
-# main function 
-# Takes in the path of the csv file and an output path 
+# main function
+# Takes in the path of the csv file and an output path
 # for creating a new JSON file as arguments
 # creates a new JSON file with formatted csv data
-
-
-
